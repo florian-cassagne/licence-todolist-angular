5bc466d95e458356cc6cd542beb6547787bab248
YohannM2 <yohann.luzi@etu.univ-amu.fr>
2022-03-07 00:31:15 +0100

    add some css magic

--- angular/src/app/four-oh-four/four-oh-four.component.ts
+++ angular/src/app/four-oh-four/four-oh-four.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-four-oh-four',
   templateUrl: './four-oh-four.component.html',
-  styleUrls: ['./four-oh-four.component.css']
+  styleUrls: ['./four-oh-four.component.css'],
+
 })
 export class FourOhFourComponent implements OnInit {
 
